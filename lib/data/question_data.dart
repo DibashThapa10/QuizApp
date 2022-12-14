7751fc30741a0ad2de02b7e79132ee1a122eeb61
dibashthapa10 <dibashthapa447@gmail.com>
2022-12-14 21:18:39 +0545

    complete question list

--- lib/data/question_data.dart
+++ lib/data/question_data.dart
@@ -7,11 +7,11 @@ QuestionModel(question:"In 1768, Captain James Cook set out to explore which oce
     "Indian Ocean": false,
     "Arctic Ocean":false ,
   },),
-  QuestionModel(question:"What is actually electricity?", answer:{
-    "A flow of water": false,
-    "A flow of air": false,
-    "A flow of electrons": true,
-    "A flow of atoms":false ,
+  QuestionModel(question:"Which among the following City was the venue of first South Asian Games?", answer:{
+    "Dhaka, Bangladesh": false,
+    "Colombo, Sri Lanka": false,
+    "Kathmandu, Nepal": true,
+    "New Delhi, India":false ,
   },),
   QuestionModel(question:"Which did Viking people use as money?", answer:{
     "Rune stones": false,
@@ -30,6 +30,36 @@ QuestionModel(question:"In 1768, Captain James Cook set out to explore which oce
     "Old Town Road": false,
     "I Don't Care": false,
     "Bad Guy":false ,
+  },),
+  QuestionModel(question:"Which of the following footballers has a world record of highest goal score for a single club?", answer:{
+    "Cristiano Ronaldo(Real Madrid FC)": false,
+    "Pele(Santos FC)": false,
+    "Gerd Muller(Bayern Munich)": false,
+    "Lionel Messi(Barcelona FC)":true ,
+  },),
+  QuestionModel(question:"What is actually electricity?", answer:{
+    "A flow of water": false,
+    "A flow of air": false,
+    "A flow of electrons": true,
+    "A flow of atoms":false ,
+  },),
+  QuestionModel(question:"Which one is the largest tropical rain forest in the world?", answer:{
+    "Bosawas": false,
+    "Amazon": true,
+    "Southeast Asian Rain Forest": false,
+    "Daintree Rain Forest":false ,
+  },),
+  QuestionModel(question:"Which river flowing through the London City, England?", answer:{
+    "Thames": true,
+    "Severn": false,
+    "Volga": false,
+    "None of the above":false ,
+  },),
+  QuestionModel(question:"Only continent in the world without a desert is?", answer:{
+    "Asia": false,
+    "South America": false,
+    "Africa": false,
+    "Europe":true ,
   },),
 
 ];
